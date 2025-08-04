2af1999303d3d8a8b3b9c09f26b35578d7c2bef6
Luís Eduardo Lemos <luis.lemos@aluno.ufr.edu.br>
2025-08-04 10:10:16 -0400

    decimo

--- server/src/routes/transactions-route.ts
+++ server/src/routes/transactions-route.ts
@@ -1,17 +1,16 @@
-import { Router } from 'express';
-import { TransactionController } from '../controllers/TransactionController.ts';
-import { TransactionService } from '../services/TransactionService.ts';
-import { TransactionRepository } from '../repositories/TransactionRepository.ts';
+import { FastifyInstance } from 'fastify';
+import {
+  createTransactionController,
+  listTransactionsController,
+  getTransactionController,
+  updateTransactionController,
+  deleteTransactionController
+} from '../controllers/transaction-controller.js';
 
-const transactionsRouter = Router();
-const transactionRepository = new TransactionRepository();
-const transactionService = new TransactionService(transactionRepository);
-const transactionController = new TransactionController(transactionService);
-
-transactionsRouter.post('/', (req, res) => transactionController.create(req, res));
-transactionsRouter.get('/', (req, res) => transactionController.findAll(req, res));
-transactionsRouter.get('/:id', (req, res) => transactionController.findById(req, res));
-transactionsRouter.patch('/:id', (req, res) => transactionController.update(req, res));
-transactionsRouter.delete('/:id', (req, res) => transactionController.delete(req, res));
-
-export { transactionsRouter };
+export async function transactionsRoutes(fastify: FastifyInstance) {
+  fastify.post('/transactions', createTransactionController);
+  fastify.get('/transactions', listTransactionsController);
+  fastify.get('/transactions/:id', getTransactionController);
+  fastify.patch('/transactions/:id', updateTransactionController);
+  fastify.delete('/transactions/:id', deleteTransactionController);
+}
